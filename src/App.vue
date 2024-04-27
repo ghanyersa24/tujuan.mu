efa5e41ca37a56b765b2589d2fe9ad1eeee63989
Ghany Ersa <ghany.ersa@sekolahmu.co.id>
2024-04-28 00:57:13 +0700

    remove vue router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
 <template>
-  <RouterView />
+  <keep-alive>
+    <component :is="routedComponent"></component>
+  </keep-alive>
 </template>
+
+<script>
+import Home from "./pages/index.vue";
+import LaporanBaksos from "./pages/laporan-baksos.vue";
+const routes = {
+  "/": Home,
+  "/laporan-baksos-2024": LaporanBaksos,
+};
+
+export default {
+  data() {
+    return { current: window.location.pathname };
+  },
+  computed: {
+    routedComponent() {
+      const pages = routes[this.current];
+      if (!pages) return Home;
+      return pages;
+    },
+  },
+};
+</script>
--- src/pages/laporan-baksos.vue
+++ src/pages/laporan-baksos.vue
@@ -4,7 +4,7 @@ setTimeout(() => {
   // redirect link
   window.location.href =
     "https://lookerstudio.google.com/reporting/b6d3d14e-81be-4dd2-a865-d5169fa49bd2/page/3lrvD";
-}, 3000);
+}, 2000);
 </script>
 
 <template>
